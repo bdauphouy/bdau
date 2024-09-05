059f5f9fc5e3d56fab6ce89f1a2a8bdf81159ca9
Baptiste Dauphouy <bdauphouy@gmail.com>
2024-09-05 19:21:56 +0200

    fix: update resumes + reduce padding bottom mobile timeline

--- src/routes/[lang]/+page.svelte
+++ src/routes/[lang]/+page.svelte
@@ -120,7 +120,7 @@
 
 <section
 	id="timeline-section"
-	class="flex h-screen flex-col-reverse justify-start gap-12 py-32 pt-6 md:gap-20 md:pt-12 lg:grid lg:h-auto lg:grid-cols-2"
+	class="flex h-screen flex-col-reverse justify-start gap-12 py-20 pt-6 md:gap-20 md:pt-12 lg:grid lg:h-auto lg:grid-cols-2 lg:py-32"
 >
 	{#if isTimelineItemContentExpanded}
 		<TimelineContent
