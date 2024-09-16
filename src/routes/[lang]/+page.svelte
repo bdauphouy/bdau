de37a46440273d2eff6854207fcf98c67693f6aa
Baptiste Dauphouy <bdauphouy@gmail.com>
2024-09-16 17:54:55 +0200

    feat: smooth scrolling

--- src/routes/[lang]/+page.svelte
+++ src/routes/[lang]/+page.svelte
@@ -4,11 +4,25 @@
 	import Landing from '$lib/components/sections/landing.svelte';
 	import Projects from '$lib/components/sections/projects.svelte';
 	import Timeline from '$lib/components/sections/timeline.svelte';
+	import { gsap } from 'gsap';
 	import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
-	import { onDestroy } from 'svelte';
+	import Lenis from 'lenis';
+	import { onDestroy, onMount } from 'svelte';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
+
+	onMount(() => {
+		const lenis = new Lenis();
+
+		lenis.on('scroll', ScrollTrigger.update);
+
+		gsap.ticker.add((time) => {
+			lenis.raf(time * 1000);
+		});
+
+		gsap.ticker.lagSmoothing(0);
+	});
 
 	onDestroy(() => {
 		ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
